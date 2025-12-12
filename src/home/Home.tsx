c0064824a4f1f6ed57daa218c6605dd5828d3f50
Marco Sanfratello <marco.sanfratello@karakun.com>
2025-12-12 22:24:08 +0100

    show only board games and filter out boardgameexpansion

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -16,53 +16,55 @@ export function Home() {
   const collection: Collection = useCollectionContext();
   const links: Links = useLinksContext();
 
-  const items: Item[] = collection.map((i) => {
-    const bestWith = i["poll-summary"].result.find(p => p.name === 'bestwith');
-    const value = bestWith?.value || '';
-    const best = [...value.match(/(\d\+|\d+-\d+|\d+)/g) || []];
-    const bestPlayers = best.join(', ');
-    return ({
-      id: i.id,
-      externalSrc: `https://boardgamegeek.com/boardgame/${i.id}/`,
-      rating: ((i.statistics.ratings.ranks.rank as Rank)?.value || (i.statistics.ratings.ranks.rank as Rank[])?.[0].value) as number,
-      name: i.name[0].value, // todo: first is probably always primary?
-      year: i.yearpublished.value,
-      backgroundImage: i.image, // thumbnail | image
-      thumbnail: i.thumbnail, // thumbnail | image
-      minPlayers: i.minplayers.value,
-      maxPlayers: i.maxplayers.value,
-      bestPlayers_raw: best,
-      bestPlayers_numeric: best.flatMap(b => {
-        if (b.endsWith('+')) {
-          return [Number(b.substring(0, b.length - 1)), 99];
-        }
-        if (b.includes('-')) {
-          const n = b.match(/\d+/g) || [];
-          const arr = [];
-          for (let j = Number(n[0]); j <= Number(n[1]); j++) {
-            arr.push(j);
+  const items: Item[] = collection
+    .filter(i => i.type === 'boardgame')
+    .map((i) => {
+      const bestWith = i["poll-summary"].result.find(p => p.name === 'bestwith');
+      const value = bestWith?.value || '';
+      const best = [...value.match(/(\d\+|\d+-\d+|\d+)/g) || []];
+      const bestPlayers = best.join(', ');
+      return ({
+        id: i.id,
+        externalSrc: `https://boardgamegeek.com/boardgame/${i.id}/`,
+        rating: ((i.statistics.ratings.ranks.rank as Rank)?.value || (i.statistics.ratings.ranks.rank as Rank[])?.[0].value) as number,
+        name: i.name[0].value, // todo: first is probably always primary?
+        year: i.yearpublished.value,
+        backgroundImage: i.image, // thumbnail | image
+        thumbnail: i.thumbnail, // thumbnail | image
+        minPlayers: i.minplayers.value,
+        maxPlayers: i.maxplayers.value,
+        bestPlayers_raw: best,
+        bestPlayers_numeric: best.flatMap(b => {
+          if (b.endsWith('+')) {
+            return [Number(b.substring(0, b.length - 1)), 99];
           }
-          return arr;
-        }
-        return Number(b);
-      }),
-      bestPlayers: bestPlayers,
-      weight_raw: i.statistics.ratings.averageweight.value,
-      weight: Weight[Math.round(i.statistics.ratings.averageweight.value)],
-      caption: '',
-      tags: i.link
-        .filter(l => ['boardgamecategory', 'boardgamemechanic'].includes(l.type))
-        .reduce((set: string[], l) => {
-          if (!set.includes(l.value)) {
-            set.push(l.value);
+          if (b.includes('-')) {
+            const n = b.match(/\d+/g) || [];
+            const arr = [];
+            for (let j = Number(n[0]); j <= Number(n[1]); j++) {
+              arr.push(j);
+            }
+            return arr;
           }
-          return set;
-        }, [])
-        .sort((l1, l2) => links[l2] - links[l1])
-        .slice(0, 6)
-      , links: i.link
-    });
-  })
+          return Number(b);
+        }),
+        bestPlayers: bestPlayers,
+        weight_raw: i.statistics.ratings.averageweight.value,
+        weight: Weight[Math.round(i.statistics.ratings.averageweight.value)],
+        caption: '',
+        tags: i.link
+          .filter(l => ['boardgamecategory', 'boardgamemechanic'].includes(l.type))
+          .reduce((set: string[], l) => {
+            if (!set.includes(l.value)) {
+              set.push(l.value);
+            }
+            return set;
+          }, [])
+          .sort((l1, l2) => links[l2] - links[l1])
+          .slice(0, 6)
+        , links: i.link
+      });
+    })
 
   return (
     <>
