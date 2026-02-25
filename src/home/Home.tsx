a034b0c9cee4705caa5c520e8188ff9312381317
Marco Sanfratello <sanfratello.m@gmail.com>
2026-02-25 09:29:42 +0100

    use bayesavg instead of rank

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -24,11 +24,17 @@ export function Home() {
       const value = bestWith?.value || '';
       const best = [...value.match(/(\d\+|\d+-\d+|\d+)/g) || []];
       const bestPlayers = best.join(', ');
+      const names = Array.isArray(i.name) ? i.name : [i.name];
+      const tagSet = new Set(
+        (i.link || [])
+          .filter(l => ['boardgamecategory','boardgamemechanic'].includes(l.type))
+          .map(l => l.value)
+      );
       return ({
         id: i.id,
         externalSrc: `https://boardgamegeek.com/boardgame/${i.id}/`,
-        rating: ((i.statistics.ratings.ranks.rank as Rank)?.value || (i.statistics.ratings.ranks.rank as Rank[])?.[0].value) as number,
-        name: (i.name as CollectionName[])[0]?.value || (i.name as CollectionName).value, // todo: first is probably always primary?
+        rating: a.statistics.ratings.bayesaverage.value,
+        name: names[0]?.value, // todo: first is probably always primary?
         year: i.yearpublished.value,
         backgroundImage: i.image, // thumbnail | image
         thumbnail: i.thumbnail, // thumbnail | image
@@ -37,33 +43,24 @@ export function Home() {
         bestPlayers_raw: best,
         bestPlayers_numeric: best.flatMap(b => {
           if (b.endsWith('+')) {
-            return [Number(b.substring(0, b.length - 1)), 99];
+            return [Number(b.slice(0, -1)), 99];
           }
           if (b.includes('-')) {
-            const n = b.match(/\d+/g) || [];
+            const [start, end] = b.match(/\d+/g)!.map(Number);
             const arr = [];
-            for (let j = Number(n[0]); j <= Number(n[1]); j++) {
-              arr.push(j);
-            }
+            for (let j = start; j <= end; j++) arr.push(j);
             return arr;
           }
-          return Number(b);
+          return [Number(b)];
         }),
         bestPlayers: bestPlayers,
         weight_raw: i.statistics.ratings.averageweight.value,
         weight: Weight[Math.round(i.statistics.ratings.averageweight.value)],
         caption: '',
-        tags: i.link
-          .filter(l => ['boardgamecategory', 'boardgamemechanic'].includes(l.type))
-          .reduce((set: string[], l) => {
-            if (!set.includes(l.value)) {
-              set.push(l.value);
-            }
-            return set;
-          }, [])
-          .sort((l1, l2) => links[l2] - links[l1])
-          .slice(0, 6)
-        , links: i.link
+        tags: Array.from(tagSet)
+          .sort((a, b) => (links[b] || 0) - (links[a] || 0))
+          .slice(0, 6),
+        links: i.link
       });
     })
 
